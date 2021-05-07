f8d3d2b8c0a4b55919722e8b58e83678290dc4a9
Nagavarma21 <60417182+Nagavarma21@users.noreply.github.com>
2021-05-08 02:55:24 +0530

    default

--- src/main/java/com/spring/DependencyInjection/Service/i18nGreetingService.java
+++ src/main/java/com/spring/DependencyInjection/Service/i18nGreetingService.java
@@ -3,7 +3,7 @@ package com.spring.DependencyInjection.Service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ENPARENT")
+@Profile({"ENPARENT","default"})
 @Service("i18Service")
 public class i18nGreetingService implements GreetingService{
     @Override
